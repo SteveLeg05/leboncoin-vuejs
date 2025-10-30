78bf30c4941c796aecff28ac1bf3fddb73078601
SteveLeg05 <stevleg0880@gmail.com>
2025-10-30 18:28:02 +0100

    added homeview page

---
+++ src/components/Card.vue
@@ -0,0 +1,51 @@
+<script setup>
+const props = defineProps({
+  avatar: String,
+  username: String,
+  picture: String,
+  title: String,
+  price: Number,
+  date: String,
+})
+</script>
+<template>
+  <main>
+    <section class="offerCard">
+      <div>
+        <img :src="avatar" alt="avatar du propriétaire" />
+        <p>{{ username }}</p>
+      </div>
+
+      <img :src="picture" alt="image de l'article" />
+      <h3>{{ title }}</h3>
+      <p>{{ price }}</p>
+      <p>{{ date }}</p>
+    </section>
+  </main>
+</template>
+<style script>
+.offerCard {
+  border: solid 2px green;
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+  height: 300px;
+}
+
+.offerCard > div {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+
+.offerCard > div img {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+}
+
+.offerCard > div p {
+  font-size: 18px;
+  font-weight: bold;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,22 @@
-<script setup></script>
+<script setup>
+import axios from 'axios'
+import { onMounted, ref } from 'vue'
+import Card from '@/components/Card.vue'
+
+const offersArray = ref([])
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get(
+      `https://site--strapileboncoin--2m8zk47gvydr.code.run/api/offers?populate=pictures,owner.avatar`,
+    )
+    offersArray.value = data.data
+    console.log(offersArray.value[0])
+  } catch (error) {
+    console.log(`catch >>>`, error)
+  }
+})
+</script>
 
 <template>
   <main class="container">
@@ -12,6 +30,17 @@
 
       <img src="../assets/img/feuille-bleue.svg" alt="feuille bleue" />
     </div>
+
+    <Card
+      v-for="offer in offersArray"
+      :key="offer.id"
+      :avatar="offer.attributes.owner?.data?.attributes?.avatar?.data?.attributes?.url"
+      :username="offer.attributes.owner.data.attributes.username"
+      :picture="offer.attributes.pictures?.data[0]?.attributes?.url"
+      :title="offer.attributes.title"
+      :price="offer.attributes.price"
+      :date="offer.attributes.createdAt"
+    />
   </main>
 </template>
 
